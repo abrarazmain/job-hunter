db89338e2b0e3eab6e9e82b7181f93d0e9141c9d
abrarazmain <ajmainabrar5@gmail.com>
2023-04-10 12:21:21 +0600

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="my-container flex justify-between main-bg">
-      <div className="main-test-color ">CarearHub</div>
-      <div className="">
+    <div className="px-4 py-4 mx-auto  sm:max-w-xl md:max-w-full  md:px-24 lg:px-72 py-6 flex justify-between main-bg rounded-lg">
+      <div className="main-test-color nav-title ">JOBHUNTER</div>
+      <div className="nav-link">
         <Link className="nav">Statics</Link>
         <Link className="nav">Applied Job</Link>
         <Link className="nav">Blog</Link>
       </div>
       <div>
-        <button className="main-btn-color px-4 py-3">Start Applying</button>
+        <button className="main-btn-color px-4 py-2">Start Applying</button>
       </div>
     </div>
   );
